b19951e7e68e0809063300d7963818506fb365ca
ZezetuSibara <123359014+ZezetuSibara@users.noreply.github.com>
2024-10-05 20:16:08 +0200

    Updated JS file

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,7 +1,4 @@
-// JavaScript source code
-// Importing Moment.js and Moment Timezone
-import moment from "moment";
-
+// Function to update Los Angeles and Paris clocks
 function updateTime() {
     // Los Angeles
     let losAngelesElement = document.querySelector("#los-angeles");
@@ -10,7 +7,7 @@ function updateTime() {
         let losAngelesTimeElement = losAngelesElement.querySelector(".time");
         let losAngelesTime = moment().tz("America/Los_Angeles");
 
-        losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
+        losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
         losAngelesTimeElement.innerHTML = losAngelesTime.format(
             "h:mm:ss [<small>]A[</small>]"
         );
@@ -23,36 +20,42 @@ function updateTime() {
         let parisTimeElement = parisElement.querySelector(".time");
         let parisTime = moment().tz("Europe/Paris");
 
-        parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+        parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
         parisTimeElement.innerHTML = parisTime.format(
             "h:mm:ss [<small>]A[</small>]"
         );
     }
 }
 
+// Function to add a new city
 function updateCity(event) {
     let cityTimeZone = event.target.value;
     if (cityTimeZone === "current") {
         cityTimeZone = moment.tz.guess();
     }
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = `
-  <div class="city">
-    <div>
-      <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-        "A"
-    )}</small></div>
-  </div>
-  `;
+
+    if (cityTimeZone) {
+        let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+        let cityTime = moment().tz(cityTimeZone);
+        let citiesElement = document.querySelector("#cities");
+        let cityElement = `
+        <div class="city">
+            <div>
+                <h2>${cityName}</h2>
+                <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            </div>
+            <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+        </div>
+        `;
+        citiesElement.insertAdjacentHTML("beforeend", cityElement);
+    }
 }
 
+// Initial update for Los Angeles and Paris clocks
 updateTime();
 setInterval(updateTime, 1000);
 
+// Event listener for city selection
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
+
